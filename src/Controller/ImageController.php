318b785976c65c81b28bd38c350c7d12fec38b9c
HericlisM <HericlisMartins@users.noreply.github.com>
2020-05-15 03:26:14 +1000

    backend/Imagecontroller fix/Return {max

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -9,10 +9,6 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
-
 /**
  * @Route("/api/image", name="image")
  */
@@ -37,24 +33,34 @@ class ImageController extends AbstractController
     }
 
     /**
-     * @Route(  "/imgur/{keyword}", 
+     * @Route(  "/imgur/{max}/{keyword}", 
      *          name="Getimgur",
-     *          requirements={ "keyword": "\w+" }
+     *          requirements={ "max": "\d+" }
      *       )
      */
-    public function Getimgur($keyword)
+    public function Getimgur($keyword, $max)
     {
-
         /** TODO:  VALIDATE THE KEYWORD BEFORE PARSE TO THE IMFURAPI */
         $data = $this->ImgurHelper($keyword);
 
+        $array_response = array();
+
         foreach ($data['data'] as $gallery_key => $gallery) {
-            if (isset($gallery['images'])) {
-                foreach ($gallery['images'] as $image) {
-                    $array_response[] = array("title" => $image['title'], "description" => $image['description'], "url" => $image['link']);
+            /*FROM ALBUNS*/
+            if ($gallery['is_album']) {
+                $image = $gallery['images'][0];
+                if ($image["type"] == "image/jpeg") {
+                    $description = substr($image['description'], 0, 30);
+                    $array_response[] = array("title" => $gallery['title'], "description" => $description, "url" => $image['link']);
                 }
             }
+
+            if (count($array_response) == $max)
+                break;
         }
+
+        if (count($array_response) <= 0)
+            $array_response[] = array("error" => "No results");
 
         $response = new Response(
             json_encode($array_response),
